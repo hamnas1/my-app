ba204971d6dace1de3224b6bae786a7893ef3f49
hamnas1 <hamnasaleemm1@gmail.com>
2024-07-09 20:51:21 +0500

    increment added

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,3 +1,21 @@
-export default function Home() {
-    return (<p className=" text-lg"> hello world! portfolio </p> );
+
+
+
+export default function Home()
+{  var count=0
+
+ const decrement =() => {
+   count=(count-1)
+}
+const increment =() =>
+{
+   count = (count+1)
+}
+
+    return (<> <p className=" text-lg"> hello world! portfolio </p> 
+    <button onClick={(e) => {increment();}}> + </button>
+    <button onClick={(e) => {decrement();}}> - </button>
+    
+    </> );
   }
+  
