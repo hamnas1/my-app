1c873f301764aa23de140c9fcdec70aff2c6ffe4
hashirshoaeb <hashirshoaeb@gmail.com>
2024-07-10 12:39:05 +0500

    styling fix

--- app/portfolio/Incrementer.tsx
+++ app/portfolio/Incrementer.tsx
@@ -3,28 +3,29 @@
 import React, { useState } from "react";
 
 export function Incrementer() {
- // var count = 2
- const [count, setCount]= useState(0);
+  // var count = 2
+  const [count, setCount] = useState(0);
   const decrement = () => {
-   // count = (count - 1)
-   setCount((prevState)=>{
-    return prevState-1;
-   });
-    console.log('count ',count)
+    // count = (count - 1)
+    setCount((prevState) => {
+      return prevState - 1;
+    });
+    console.log('count ', count)
   }
 
   const increment = () => {
-   // count = (count + 1)
-   setCount((prevState)=>{
-    return prevState+1;
-   });
-    console.log('count ',count)
+    // count = (count + 1)
+    setCount((prevState) => {
+      return prevState + 1;
+    });
+    console.log('count ', count)
   }
 
   return (
     <>
       <p className=" text-lg"> hello world! portfolio </p>
       <p> {count}</p>
+      <p className=" bg-slate-400">hii</p>
       <button onClick={(e) => { increment(); }}> + </button>
       <button onClick={(e) => { decrement(); }}> - </button>
     </>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,51 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
+// https://stackoverflow.com/a/76919264/16220781
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './ui/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      gridTemplateColumns: {
+        '13': 'repeat(13, minmax(0, 1fr))',
+      },
+      colors: {
+        blue: {
+          400: '#2589FE',
+          500: '#0070F3',
+          600: '#2F6FEB',
+        },
+      },
+    },
+    keyframes: {
+      shimmer: {
+        '100%': {
+          transform: 'translateX(100%)',
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    // require('@tailwindcss/forms')({ strategy: 'class' }),
+    // require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["light"]
+  }
 };
 export default config;
