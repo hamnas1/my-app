15308df5d6ec02e9c854a1dce427ba9594767a8c
hamnas1 <hamnasaleemm1@gmail.com>
2024-07-10 15:15:54 +0500

    New design page

---
+++ app/portfolio/design.tsx
@@ -0,0 +1,25 @@
+const DesignPage = () => {
+  return (
+    <>
+      <h3 className="  pl-80 text-5xl w-945px h-54px pb-4  font-roboto pr-415 pt-32 text-48px font-medium tracking-0.4px text-[#171717] text-left align-middle leading-1.13 flex-grow-0">
+        {" "}
+        A{" "}
+        <b className="text-5xl text-blue-600  w-945px h-54px  font-roboto pb-4 text-48px font-medium tracking-0.4px text-left align-middle pt-32 leading-1.13 flex-grow-0">
+          community
+        </b>{" "}
+        of passionate Flutter learners{" "}
+      </h3>
+      <p className=" pl-80 pr-80 pt-1280 h-120px text-left self-stretch flex-grow-0 opacity-85 text-xl font-roboto font-normal not-italic leading-6 tracking-normal text-[#5f5f5f] ">
+        {" "}
+        Our mission is to make learning Flutter accessible and fun for everyone.
+        Comprehensive, interactive, and practical learning experience. Join a
+        community of passionate learners, help each other grow, and build
+        amazing applications together. Our mission is to make learning Flutter
+        accessible and fun for everyone. Comprehensive, interactive, and
+        practical learning experience. Join a community of passionate learners,
+        help each other grow, and build amazing applications together.
+      </p>
+    </>
+  );
+};
+export default DesignPage;
--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,9 +1,10 @@
+import DesignPage from "./design";
 import { Incrementer } from "./Incrementer";
 
 export default async function Home() {
   return (
     <>
-      <Incrementer />
+      <DesignPage />
     </>
   );
 }
